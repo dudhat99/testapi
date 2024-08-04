36f5e3407244c8e37704986f8a1fb189a35d33e3
dudhat99 <chetan.dudhat99@gmail.com>
2024-08-04 17:32:39 +0530

    checkin

---
+++ TestAPI/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
